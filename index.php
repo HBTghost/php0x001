a83d4cbd8984f4dac1fc12252d521373b3bb4b96
HBTghost <hacbangtam@gmail.com>
2020-04-18 00:25:10 +0700

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Welcome, Brian</title>
+</head>
+<body>
+  <?php 
+    echo("<h1>Hello World</h1>");
+  ?>
+</body>
+</html>
